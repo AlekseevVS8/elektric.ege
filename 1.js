3f939d5914f40e80de23d2710c6266810e48d71b
AlekseevVS8 <125433511+AlekseevVS8@users.noreply.github.com>
2023-03-03 01:05:02 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -5,3 +5,17 @@ if ( calc.answer.value == "6" )
      alert("Правильно!");
 else alert("Неправильно!");
 }
+
+function check()
+{
+if ( calc.n2.value == "30" )
+     alert("Правильно!");
+else alert("Неправильно!");
+}
+
+function check()
+{
+if ( calc.n3.value == "2" )
+     alert("Правильно!");
+else alert("Неправильно!");
+}
