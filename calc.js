c6d44b0ec73e9b86be0d9ea5dcf78c0b5f732bbc
AlekseevVS8 <125433511+AlekseevVS8@users.noreply.github.com>
2023-03-07 10:52:22 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -4,3 +4,15 @@ if ( calc.answer.value == "6" )
 	alert("Правильно!");
 else alert("Неправильно!"); 
 }
+function ch2() 
+{
+if ( calc.answer.value == "30" ) 
+	alert("Правильно!");
+else alert("Неправильно!"); 
+}
+function ch3() 
+{
+if ( calc.answer.value == "2" ) 
+	alert("Правильно!");
+else alert("Неправильно!"); 
+}
